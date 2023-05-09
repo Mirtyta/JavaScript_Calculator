cdb8bd3b19cd838adf80704045e5741b0ff40f67
Mirta Gomez <mirta_gomez@yahoo.com.ar>
2023-05-09 19:35:52 -0300

    Primera Prueba
    es una calculadora en java muy rudimentaria, la idea es hacerla mas visualmente linda y que funcione como una calculadora real, con botones y todo eso.

---
+++ tuto.js
@@ -0,0 +1,39 @@
+console.log("javascript conectado exitosamente!")
+const txtOperador1 = document.getElementById("op1")
+const txtoperacion = document.getElementById("operacion")
+const txtOperador2 = document.getElementById("op2")
+const btnCalcular = document.getElementById("calcular")
+const txtResultado = document.getElementById("resultado")
+
+btnCalcular.addEventListener('click',calcular)
+
+function calcular(){
+ console.log("Apretaste el boton calcular")
+ console.log(txtoperacion.value)
+ const operacion = txtoperacion.value
+ const operador1 = parseFloat(txtOperador1.value)
+ const operador2 = parseFloat(txtOperador2.value)
+ 
+ if((operacion == "+" || operacion == "-" || operacion == "*"  || operacion == "/") && !isNaN(operador1) && !isNaN(operador2)){
+  let resultado
+  switch(operacion){
+   case "+":
+    resultado = operador1 + operador2
+    break;
+   case "-":
+    resultado = operador1 - operador2
+    break;
+   case "*":
+    resultado = operador1 * operador2
+    break;
+   case "/":
+    resultado = operador1 / operador2
+    break;
+  }
+
+  txtResultado.innerText = "Resultado es:" + resultado
+ }else{
+  txtResultado.style.backgroundColor="red"
+  txtResultado.innerText = "Calculo imposible. Debe colocar números y operadores válidos"
+ }
+}
